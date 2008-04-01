326afdfbe981aed54eb7aa984b2d2636ec9d71e0
Glen Smith <glenasmith@users.noreply.github.com>
2008-04-01 10:08:51 +0000

    Fixes for the move to Grails 1.0.2

--- grails-app/jobs/UpdateFeedsJob.groovy
+++ grails-app/jobs/UpdateFeedsJob.groovy
@@ -2,16 +2,34 @@
 class UpdateFeedsJob {
 	
 	FeedService feedService
-	
-	// def cronExpression = "0 * * * * ?"
+
+    private static updating = false
+
+    // def cronExpression = "0 * * * * ?"
 	// use default to fetch every minute		
 
-	def execute() {	
-		
-		log.info("Starting scheduled feed check at: " + new Date())
-	
-		feedService.updateFeeds()
-	    
-	    log.info("Finished scheduled feed check at: " + new Date())
-	}
+	def execute() {
+
+        if (!updating) {
+
+            updating = true
+
+            log.info("Starting scheduled feed check at: " + new Date())
+
+            try {
+                feedService.updateFeeds()
+            } catch (Throwable t) {
+                log.error("Error updating feeds", t)
+            }
+
+            updating = false
+
+            log.info("Finished scheduled feed check at: " + new Date())
+
+        } else {
+
+            log.warn("\n\n>>>>Skipping Feed update. Previous job in progress\n\n")
+
+        }
+    }
 }
