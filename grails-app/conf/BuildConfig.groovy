b97e8eda1445fafc6a6457eb7185b0e32fdd2f26
Soeren Berg Glasius <soeren@glasius.dk>
2014-09-01 16:36:10 -0700

    Made dependencies in build work

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,41 +1,98 @@
+grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir	= "target/test-reports"
+grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target/work"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+//grails.project.fork = [
+//    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+//    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+//
+//    // configure settings for the test-app JVM, uses the daemon by default
+//    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+//    // configure settings for the run-app JVM
+//    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+//    // configure settings for the run-war JVM
+//    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+//    // configure settings for the Console UI JVM
+//    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+//]
+
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
-    inherits( "global" ) {
-        // uncomment to disable ehcache
+    inherits("global") {
+        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
         // excludes 'ehcache'
     }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {        
+    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    checksums true // Whether to verify checksums on resolve
+    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+
+    repositories {
+        inherits true // Whether to inherit repository definitions from plugins
+
         grailsPlugins()
         grailsHome()
-
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
+        mavenLocal()
+        grailsCentral()
         mavenCentral()
-        mavenRepo "http://download.java.net/maven/2/"
-        mavenRepo "http://developer.jasig.org/repo/content/groups/m2-legacy/"
-        mavenRepo "https://m2proxy.atlassian.com/repository/public/"
-        
-        //mavenRepo "http://snapshots.repository.codehaus.org"
+        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
+
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        compile 'jfree:jfreechart:1.0.4'
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
+        runtime 'mysql:mysql-connector-java:5.1.29'
+        // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
+         compile 'jfree:jfreechart:1.0.4'
         compile 'net.homeip.yusuke:twitter4j:2.0.10'
         compile 'rome:rome:1.0'
         compile 'commons-httpclient:commons-httpclient:3.1'
-        compile 'spy:memcached:2.3.1'
-        runtime 'postgresql:postgresql:8.2-507.jdbc3'
-        
-        // runtime 'mysql:mysql-connector-java:5.1.5'
+        compile 'com.google.code.maven-play-plugin.spy:memcached:2.4.2'
+        compile 'org.compass-project:compass:2.2.0'
+                //        runtime 'postgresql:postgresql:8.2-507.jdbc3'
+
+        test "org.grails:grails-datastore-test-support:1.0-grails-2.4"
     }
 
+    plugins {
+        // plugins for the build system only
+        build ":tomcat:7.0.55"
+
+        // plugins for the compile step
+        compile ":scaffolding:2.1.2"
+        compile ':cache:1.1.7'
+        compile ":asset-pipeline:1.9.6"
+        compile ":quartz:1.0.2"
+        compile ":spring-security-core:2.0-RC4"
+        compile ":google-chart:0.5.2"
+        compile ":mail:1.0.7"
+        compile ":runtime-logging:0.4"
+        compile ":searchable:0.6.9"
+        compile ":iwebkit:0.5"
+
+        // plugins needed at runtime but not for compilation
+        runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
+        runtime ":database-migration:1.4.0"
+        runtime ":jquery:1.11.1"
+
+
+
+        // Uncomment these to enable additional asset-pipeline capabilities
+        //compile ":sass-asset-pipeline:1.9.0"
+        //compile ":less-asset-pipeline:1.10.0"
+        //compile ":coffee-asset-pipeline:1.8.0"
+        //compile ":handlebars-asset-pipeline:1.3.0.3"
+
+//        plugins.iwebkit=0.2
+//        plugins.jsecurity=0.4.1
+
+
+    }
 }
